672b8f283bd37ff5b86ad5a4aeeb1ded0d5265f0
Juan José Jaramillo <74982329+juan3312@users.noreply.github.com>
2023-02-21 17:19:27 -0500

    mejoras del index

--- js/index.js
+++ js/index.js
@@ -21,3 +21,45 @@ const aparecerImagen = (imagen) =>{
     contenedorLight.classList.add('show');
     imagenesLight.classList.add('showImage');
 }
+
+/* Carrito
+
+let cont1 = 6;
+let cont2 = 9;
+let cont3 = 10;
+
+/* 
+Expresiones equivalentes
+cont1++; 
+cont1+=1;
+cont1 = cont1 + 1;
+*/
+
+/*document.write(++cont1+"<br>");
+document.write((cont1++)+"<br>");
+document.write(cont1);*/
+
+//Probar diferentes combinaciones del operador post, pre incremento/decremento
+
+
+//Imprima los números pares entre n y m, donde n < m
+/*
+let n, m, i;
+let salida = "";
+n = 3;
+m = 81;
+
+for(i=n;i<=m;i++)
+{
+  if(i%2==0)
+  {
+    salida = salida + i + "<br>";
+  }
+}
+
+document.write(salida);*/
+
+
+
+/* galeria index*/
+
