d660c85d3c1502129e21f2e39bab2863479fb43c
Juan José Jaramillo <74982329+juan3312@users.noreply.github.com>
2023-02-22 20:07:16 -0500

    añado formulario contacto

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* Galeria index */
 const imagenes = document.querySelectorAll('.img-galeria');
 const imagenesLight = document.querySelector('.agregar-imagen');
 const contenedorLight = document.querySelector('.imagen-light');
@@ -21,45 +22,37 @@ const aparecerImagen = (imagen) =>{
     contenedorLight.classList.add('show');
     imagenesLight.classList.add('showImage');
 }
+ /* fin galeria index*/
 
-/* Carrito
+ /* contact */
+ $(document).ready(function(){
 
-let cont1 = 6;
-let cont2 = 9;
-let cont3 = 10;
-
-/* 
-Expresiones equivalentes
-cont1++; 
-cont1+=1;
-cont1 = cont1 + 1;
-*/
-
-/*document.write(++cont1+"<br>");
-document.write((cont1++)+"<br>");
-document.write(cont1);*/
-
-//Probar diferentes combinaciones del operador post, pre incremento/decremento
-
-
-//Imprima los números pares entre n y m, donde n < m
-/*
-let n, m, i;
-let salida = "";
-n = 3;
-m = 81;
-
-for(i=n;i<=m;i++)
-{
-  if(i%2==0)
-  {
-    salida = salida + i + "<br>";
-  }
-}
-
-document.write(salida);*/
-
-
-
-/* galeria index*/
-
+	//material contact form
+	$('.contact-form').find('.form-control').each(function() {
+	  var targetItem = $(this).parent();
+	  if ($(this).val()) {
+		$(targetItem).find('label').css({
+		  'top': '10px',
+		  'fontSize': '14px'
+		});
+	  }
+	})
+	$('.contact-form').find('.form-control').focus(function() {
+	  $(this).parent('.input-block').addClass('focus');
+	  $(this).parent().find('label').animate({
+		'top': '10px',
+		'fontSize': '14px'
+	  }, 300);
+	})
+	$('.contact-form').find('.form-control').blur(function() {
+	  if ($(this).val().length == 0) {
+		$(this).parent('.input-block').removeClass('focus');
+		$(this).parent().find('label').animate({
+		  'top': '25px',
+		  'fontSize': '18px'
+		}, 300);
+	  }
+	})
+	
+});
+ /* fin contact*/
